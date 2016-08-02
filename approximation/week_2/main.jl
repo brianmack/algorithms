9f713a086404f92f414b9b9e451332df57dadc37
brian <brian.a.mack@gmail.com>
2016-08-01 23:33:08 -0400

    renamed from do_knapsack

---
+++ approximation/week_2/main.jl
@@ -0,0 +1,26 @@
+
+
+using DataFrames
+
+import knapsack
+
+data_fname = ARGS[1];
+t = parse(Float64, ARGS[2]);
+b = parse(Float64, ARGS[3]);
+
+data = readtable(data_fname);
+
+v = convert(Array, data[:v])
+s = convert(Array, data[:s])
+
+print(string(v) * "\n")
+print(string(s) * "\n")
+
+#t = 3.1
+#b = 1
+println(knapsack.fractional_knapsack(v, s, t, b))
+
+println(v)
+print(">>>>>>>>>>>\n")
+println(knapsack.fill_knapsack(v, s, t, b))
+
