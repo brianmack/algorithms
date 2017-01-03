695efdbbf6f07bb80ddee4aaeb84a2000a6c768c
brianmack <brian.a.mack@gmail.com>
2017-01-02 23:12:43 -0500

    expanded usability of main script before branching - should be ignored but keeping for posterity as it implements the homework problem

--- approximation/week_2/main.jl
+++ approximation/week_2/main.jl
@@ -1,26 +1,58 @@
+#"""
+#Executing script for knapsack examples. 
+
+#Invocation:
+
+#julia main.jl objects0.txt 5 10
+
+#"""
+
+
+
+
+# in case not set in .juliarc
+push!(LOAD_PATH, "./")
 
 
 using DataFrames
 
 import knapsack
 
+
+required_args_str = """
+    1. (str) filename to data in csv format (v=value, s=size);
+        other columns will be read but discarded
+    2. (int) target (capacity of knapsack)
+    3. (int) capacity of knapsack
+"""
+
+if length(ARGS) < 3
+    print("missing required args:\n", required_args_str)
+    println("exiting...")
+    exit(1)
+end
+
+# 'b' because 'B' was the capacity of knapsack in lectures
+
 data_fname = ARGS[1];
 t = parse(Float64, ARGS[2]);
 b = parse(Float64, ARGS[3]);
 
+println("reading data file...")
 data = readtable(data_fname);
 
 v = convert(Array{Float64}, data[:v])
 s = convert(Array{Float64}, data[:s])
 
+println("initial values and sizes:")
 print(string(v) * "\n")
 print(string(s) * "\n")
 
-#t = 3.1
-#b = 1
+println("fractional knapsack:")
 println(knapsack.fractional_knapsack(v, s, t, b))
 
 println(v)
+println("fill knapsack:")
 print(">>>>>>>>>>>\n")
-println(knapsack.fill_knapsack(v, s, t, b))
+println(knapsack.fill_knapsack(v, s, b))
 
